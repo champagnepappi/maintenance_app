4cdbf2100c6da61ff1929dd87a9b62ae2575fca9
kevin <kevinochieng548@gmail.com>
2016-07-06 21:03:23 +0300

    add users resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get 'help'   => 'static_pages#help'
 
+  resource :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
