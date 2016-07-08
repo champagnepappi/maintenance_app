25d9f75fa4ae2c2e21d62e679afcb4d24a883caa
kevin <kevinochieng548@gmail.com>
2016-07-08 21:00:23 +0300

    create login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,11 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+    else
+      render 'new'
+    end
+  end
 end
