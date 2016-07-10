32998bd22bd3872fd43183a6399653eac452cbce
kevin <kevinochieng548@gmail.com>
2016-07-10 12:54:36 +0300

    log in user upon signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
       render 'new'
       flash.now[:alert] = "Incorrect name and password combination"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       redirect_to @user
       flash[:success] = "You successfully signed up"
     else
@@ -17,6 +18,9 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def index
+    @users = User.all
+  end
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
