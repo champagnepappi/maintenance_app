c93b786143c26baf73d9161f15041f3a036ca159
kevin <kevinochieng548@gmail.com>
2016-07-10 21:44:07 +0300

    add log_in_as helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,11 +5,33 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  
+
   #make the helper method(logged_in) available
   #in test but with different name(is_logged_in)
   #to prevent being mistaken from each other
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  #logs in a test user
+  def log_in_as(user, options = {})
+    password = options[:password] || 'password'
+    remember_me = options[:remember_me] || '1'
+
+    if integration_test?
+      post login_path, session: {
+        name: user.name,
+        password: password,
+        remember_me: remember_me
+      }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+
+  private
+  def integration_test?
+    defined?(post_via_redirect)
   end
 end
