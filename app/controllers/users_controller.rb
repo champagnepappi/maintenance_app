33c8924059e5a4a5aa208d5aebc6d8722f208980
kevin <kevinochieng548@gmail.com>
2016-07-12 20:30:01 +0300

    add update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,13 @@ class UsersController < ApplicationController
   def edit
     @user = User.find_by(id: params[:id])
   end
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update_attributes(user_params)
+    else
+      render 'edit'
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
