2dab266250c5d1d9bf8e66cde7f6bfc5a19ef2b0
kevin <kevinochieng548@gmail.com>
2016-07-13 14:55:44 +0300

    add store location to logged_in_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit,:update]
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -45,9 +46,14 @@ class UsersController < ApplicationController
   #confirm a logged in user
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:alert] = "Please Login"
       redirect_to login_url
     end
+  end
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 
   
