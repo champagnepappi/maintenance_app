2331d7128923559901934892796a137bc267ea32
kevin <kevinochieng548@gmail.com>
2016-07-14 11:34:36 +0300

    add account activation to users signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      redirect_to @user
-      flash[:success] = "You successfully signed up"
-
+      # log_in @user
+      # redirect_to @user
+      # flash[:success] = "You successfully signed up"
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Check your email to activate your account"
+      redirect_to root_url
     else
       render 'new'
     end
