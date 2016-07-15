24f18150b51fbe083531357886132f8abd61de04
kevin <kevinochieng548@gmail.com>
2016-07-15 16:47:38 +0300

    test character length for content

--- test/models/request_test.rb
+++ test/models/request_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class RequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:santos)
+    @request = Request.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "user id should be present" do
+    @request.user_id = nil
+    assert_not @request.valid?
+  end
+
+  test "request should be at most 140 characters" do
+    @request.content = 'a'*201
+    assert_not @request.valid?
+  end
 end
