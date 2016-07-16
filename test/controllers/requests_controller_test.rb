3b5760f8cff95a076676cdd7d11e047f662d3c8b
kevin <kevinochieng548@gmail.com>
2016-07-16 16:54:01 +0300

    write authorization tests

--- test/controllers/requests_controller_test.rb
+++ test/controllers/requests_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class RequestsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @request = requests(:one)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Request.count' do
+      post :create, request: {content: "Lorem ipsum"}
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Request.count' do
+      delete :destroy, id: @request
+    end
+    assert_redirected_to login_url
+  end
 end
+
+ 
