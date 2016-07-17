e176849d8320df8baada201c51717788eb0b5c74
kevin <kevinochieng548@gmail.com>
2016-07-17 20:25:05 +0300

    add custom validation for image size

--- app/models/request.rb
+++ app/models/request.rb
@@ -5,4 +5,13 @@ class Request < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 200}
+  validate :picture_size
+
+  private
+  #validate size of an uploaded image
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "File too large(should be less than 5MB)")
+    end
+  end
 end
