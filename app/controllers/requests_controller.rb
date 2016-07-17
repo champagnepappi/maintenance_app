b6a9dd6fb59c0ad8140c9ee02faf794746c89923
kevin <kevinochieng548@gmail.com>
2016-07-17 11:09:04 +0300

    define destroy action to enable deleting of requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,6 @@
 class RequestsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @request = current_user.requests.build(request_params)
@@ -7,16 +8,25 @@ class RequestsController < ApplicationController
       flash[:success] = "Maintenance Request created!" 
       redirect_to root_url
     else
-      @feed_items = []
+      @feed_items = [] #supress feed for failed submissions
       render 'static_pages/home'
     end
   end
 
   def destroy
+    @request.destroy
+    flash[:success] = "Maintenance Request deleted"
+    redirect_to request.referrer || root_url
+    #arrange to redirect to page issuing the delete request
   end
 
   private
   def request_params
     params.require(:request).permit(:content)
+  end
+
+  def correct_user
+    @request = current_user.requests.find_by(id: params[:id])
+    redirect_to root_url if @request.nil?
   end
 end
