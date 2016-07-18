6701896b0c4ce856ab6ec7aba717003db956fdd8
kevin <kevinochieng548@gmail.com>
2016-07-18 14:45:59 +0300

    order comments with default scope

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :request
+  default_scope -> {order(created_at: :desc)}
   validates :user_id, presence: true
   validates :request_id, presence: true
   validates :content, presence: true, length: {maximum: 150}
