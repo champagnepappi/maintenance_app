949b4ac66a17cd626865419e3cb91043c1ca50e8
kevin <kevinochieng548@gmail.com>
2016-07-22 17:43:00 +0300

    add enum status

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,7 @@
 class Request < ApplicationRecord
   belongs_to :user
-  has_many :users
   has_many :comments, dependent: :destroy
+  enum status: {approved: 0, rejected: 1,}
   #set order in which elements are retrieved from db
   default_scope -> {order(created_at: :desc)}
   mount_uploader :picture, PictureUploader
