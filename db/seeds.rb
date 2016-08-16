294855e21c2d70042f8618eb3fb438f486486a7b
kevin <kevinochieng548@gmail.com>
2016-08-16 22:33:39 +0300

    make approved request a link

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,5 +28,4 @@ users = User.order(:created_at).take(5)
 5.times do
   content = Faker::Lorem.sentence(5)
   users.each {|user| user.requests.create!(content: content)}
-  users.each {|user| user.comments.create!(content: content)}
 end
