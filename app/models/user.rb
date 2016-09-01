fc82c807f4a0089b464432957c799ce17c38cac6
kevin <kevinochieng548@gmail.com>
2016-09-01 23:20:53 +0300

    validate minimum name length

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :comments
   before_save :downcase_email
   before_create :create_activation_digest
-  validates :name, presence: true, length: {maximum: 40}
+  validates :name, presence: true, length: {minimum: 3,maximum: 40}
     # format:{with: /\A[-a-z]+\Z/, message: " cannot contain numbers"}
   validates :contact, presence: true, length: {maximum: 10},numericality: {only_integer: true}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
